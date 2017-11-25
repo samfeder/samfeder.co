64eb28bb11c395a6f73bcd229e22de8a7b4832cc
Sam Feder <sam.feder1@gmail.com>
2017-11-25 13:21:23 -0500

    fix meta tags

--- services/story-grabber.js
+++ services/story-grabber.js
@@ -27,7 +27,24 @@ function parseBody($) {
   $('.network-accounts').remove();
   $('.footer-notice').remove();
 
+  $('title').text('Sam Feder');
+
+  addMeta($);
   return $.html();
+}
+
+function addMeta($) {
+  $('meta').remove();
+
+  const description = $('.job.has-tooltip').text();
+
+  $('head').append('<meta name="twitter:card" content="summary">');
+  $('head').append('<meta name="twitter:domain" content="samfeder.co">');
+  $('head').append('<meta property="og:type" content="website">');
+  $('head').append('<meta property="og:image" itemprop="image primaryImageOfPage" content="https://i.stack.imgur.com/QgqKs.jpg">');
+  $('head').append('<meta name="twitter:title" property="og:title" itemprop="title name" content="Sam Feder">');
+  $('head').append(`<meta name="twitter:description" property="og:description" itemprop="description" content="${description}">`);
+  $('head').append('<meta property="og:url" content="https://samfeder.co">');
 }
 
 module.exports = getUrl;
